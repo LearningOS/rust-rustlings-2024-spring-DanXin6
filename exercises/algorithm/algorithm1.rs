7791ad1c714f11a34aa344db6bc2571e71d18820
danxin <798825105@qq.com>
2024-04-09 17:07:48 +0800

    faet: finished 100

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -7,6 +7,7 @@
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
 use std::vec::*;
+use std::cmp::{PartialOrd, PartialEq};
 
 #[derive(Debug)]
 struct Node<T> {
@@ -29,13 +30,13 @@ struct LinkedList<T> {
     end: Option<NonNull<Node<T>>>,
 }
 
-impl<T> Default for LinkedList<T> {
+impl<T: PartialEq+PartialOrd> Default for LinkedList<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T> LinkedList<T> {
+impl<T: PartialEq+PartialOrd> LinkedList<T> {
     pub fn new() -> Self {
         Self {
             length: 0,
@@ -71,12 +72,34 @@ impl<T> LinkedList<T> {
     }
 	pub fn merge(list_a:LinkedList<T>,list_b:LinkedList<T>) -> Self
 	{
-		//TODO
-		Self {
-            length: 0,
-            start: None,
-            end: None,
+		let mut list_c = LinkedList::<T>::new();
+        let mut node_a = list_a.start;
+        let mut node_b = list_b.start;
+        while node_a.is_some() && node_b.is_some() {
+            let val_a = unsafe { node_a.unwrap().as_ref().val };
+            let val_b = unsafe { node_b.unwrap().as_ref().val };
+            if val_a < val_b {
+                list_c.add(val_a);
+                node_a = unsafe { node_a.unwrap().as_ref().next };
+            } else {
+                list_c.add(val_b);
+                node_b = unsafe { node_b.unwrap().as_ref().next };
+            }
         }
+
+        while node_a.is_some() {
+            let val_a = unsafe { node_a.unwrap().as_ref().val };
+            list_c.add(val_a);
+            node_a = unsafe { node_a.unwrap().as_ref().next };
+        }
+
+        while node_b.is_some() {
+            let val_b = unsafe { node_b.unwrap().as_ref().val };
+            list_c.add(val_b);
+            node_b = unsafe { node_b.unwrap().as_ref().next };
+        }
+
+        list_c
 	}
 }
 
